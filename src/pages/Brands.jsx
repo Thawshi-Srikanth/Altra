14b751d3343476a4cf002b5fd7f3947b6474787f
Upul <upulkr4@gmail.com>
2024-02-26 22:09:40 +0530

    uh

--- src/pages/Brands.jsx
+++ src/pages/Brands.jsx
@@ -7,10 +7,10 @@ function Brands() {
       <p className="md:text-[35px] text-[25px] xl:text-[69px] lg:text-[50px] sm:text-[30px] font-rosario text-white  text-center lg:mt-[60px] md:mt-[30px] mt-[20px]  font-bold">
         Our trusted brands
       </p>
-      <div className="bg-white  opacity-90 lg:h-[150px] md:h-[90px]  sm:h-[80px] sm:mt-[30px] md:mt-[30px] mt-[20px] h-[60px] ">
+      <div className="bg-white sm:block hidden opacity-90 lg:h-[150px] md:h-[90px]  sm:h-[80px] sm:mt-[30px] md:mt-[30px] mt-[20px] h-[60px] ">
         {" "}
         
-        <div className="sm:block hidden  lg:mt-[30px]  flex lg:gap-[40px] gap-[20px] md:gap-[50px]  sm:gap-[80px] md:top-[10px] top-[5px]  justify-start [&_li]:mx-8 [&_img]:max-w-96  animate-infinite-scroll ">
+        <div className="  lg:mt-[30px]  flex lg:gap-[40px] gap-[20px] md:gap-[50px]  sm:gap-[80px] md:top-[10px] top-[5px]  justify-start [&_li]:mx-8 [&_img]:max-w-96  animate-infinite-scroll ">
           {brands.map((brand) => (
             <div
               key={brand.id}
@@ -22,7 +22,7 @@ function Brands() {
         </div>
      
       </div>
-      <div className="sm:hidden  lg:mt-[30px]  flex lg:gap-[40px] gap-[20px] md:gap-[50px]  sm:gap-[80px] md:top-[10px] top-[5px]  justify-start [&_li]:mx-8 [&_img]:max-w-96  animate-infinite-scroll ">
+      <div className=" sm:hidden lg:mt-[30px]  flex lg:gap-[40px] gap-[20px] md:gap-[50px]  sm:gap-[80px] md:top-[10px] top-[5px]  justify-start [&_li]:mx-8 [&_img]:max-w-96  animate-infinite-scroll ">
           {brands.map((brand) => (
             <div
               key={brand.id}
