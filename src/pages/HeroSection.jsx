b939da7f45ac742c3219ea74ea1623feb3cc8632
Upul <upulkr4@gmail.com>
2024-03-04 10:16:48 +0530

    mobile ui error

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -3,8 +3,8 @@ import flask from "../assets/images/HeroImages/flask.png";
 import { Link } from "react-router-dom";
 function HeroSection() {
   return (
-    <div className=" relative pb-[15px]  lg:pb-0  [background:linear-gradient(180deg,_#fff_70.9%,_rgba(2,_2,_1,_0))]  lg:h-[500px] grid grid-cols-3 lg:px-[90px]  bg-white w-full xl:mt-[-40px] top-[-0px] ">
-      <div className="col-span-2 lg:mt-[60px] mt-[20px] md:mt-[60px] relative xl:left-[35px] left-[30px] ">
+    <div className=" relative pb-[15px]  lg:pb-0   [background:linear-gradient(180deg,_#fff_70%,_rgba(2,_2,_1,_0))]  lg:h-[500px] grid grid-cols-3 lg:px-[90px]  bg-white w-full xl:mt-[-40px] top-[0px] ">
+      <div className="col-span-2 lg:mt-[60px] mt-[0px] md:mt-[60px] relative xl:left-[35px] left-[30px] ">
         <div className="lg:flex lg:flex-row 2xl:text-[69px] xl:text-[60px] lg:text-[40px] text-[20px] md:text-[40px] sm:text-[38px] font-rosario  lg:gap-[20px]">
           <span className="text-[#e18655] font-bold">Elevate Your</span>
           <span className="font-bold text-transparent [-webkit-text-stroke:0.5px_#000] lg:[-webkit-text-stroke:2px_#000] relative left-[8px] lg:left-0">
@@ -26,12 +26,12 @@ function HeroSection() {
               Inquire Now
             </button>
           </Link>
-          <p className="pb-[20px]  sm:pb-0 lg:ml-[86px] text-[9px] md:text-[12px] sm:text-[9px] font-rosario lg:text-[20px]  text-[#e18655] lg:mt-[20px] md:mt-[10px] mt-[5px] relative left-[12px] sm:ml-[-5px] md:left-5 lg:left-0">
+          <p className="pb-[20px]  sm:pb-0 lg:ml-[86px] text-[5px] md:text-[12px] sm:text-[9px] font-rosario lg:text-[20px]  text-[#e18655] lg:mt-[20px] md:mt-[10px] mt-[5px] relative left-[20px] sm:ml-[-5px] md:left-5 lg:left-0 ">
             <Link to="/products " className="">See Products</Link>
           </p>
         </div>
       </div>
-      <div className="relative xl:left-[35px] 2xl:left-0 bottom-[10px] lg:bottom-0 md:mt-[-13px] lg:mt-0 mt-[-5px]">
+      <div className="relative xl:left-[35px] 2xl:left-0 bottom-[10px] lg:bottom-0 md:mt-[-13px] lg:mt-0 mt-[-10px]">
         <img src={flask} alt="flask" className="lg:ml-[12px] xl:mt-[-40px] " />
       </div>
     </div>
