274087c3b0391609cd5f6d5f981d2d5c38022d1f
upulkr <upulkr4@gmail.com>
2024-03-19 12:07:02 +0530

    magnetic

--- src/pages/Magnetic.jsx
+++ src/pages/Magnetic.jsx
@@ -6,8 +6,11 @@ function Magnetic() {
   return (
     <div className=" w-full pr-[10px] md:pr-6 flex xl:mt-[160px] lg:mt-[100px] md:mt-[130px] lg:pb-6 mt-[50px] relative md:right-[10px] lg:right-0  md:scale-100 right-[4px] 2xl:top-[80px]  xl:top-[50px] lg:left-[-23px] xl:left-[-5px]">
       <div className="md:pt-[10px] rounded-[12px] lg:pt-0 md:left-[60px] md:w-11/12  md:h-[140px] md:rounded-[15px] lg:left-0 lg:mt-[18px] lg:px-[20px] xl:ml-[90px] 2xl:ml-[180px]  lg:ml-[60px] relative lg:rounded-[45px] md:bg-white md:bg-opacity-80 lg:h-[200px] xl:h-[250px] xl:w-[1255px] 2xl:w-[1250px]  lg:w-[1200px] magWidth w-[335px] sm:w-[500px] sm:h-[120px] h-[80px]  ">
-        
-        <img src={magnetFact} alt="" className="md:hidden relative bottom-[40px]  sm:bottom-[-16px]  sm:left-[160px]    sm:scale-125 mx-auto justify-center flex ml-[-5px] sm:ml-0" />
+        <img
+          src={magnetFact}
+          alt=""
+          className="md:hidden relative bottom-[40px]  sm:bottom-[-16px]  sm:left-[160px]    sm:scale-125 mx-auto justify-center flex ml-[-5px] sm:ml-0"
+        />
 
         <div>
           {" "}
@@ -18,7 +21,7 @@ function Magnetic() {
 
         <div className="md:block hidden ">
           {" "}
-          <p className=" xl:leading-[40px] font-rosario magP text-[10px]   md:leading-[30px] md:mt-[-10px] md:pt-[10px] lg:pt-0 lg:text-[28px] xl:text-[33px] lg:mt-[10px] lg:px-[10px]  lg:ml-[20px] md:ml-[20px] ml-[-15px] xl:mt-[0px] scale-95 md:scale-100 relative xl:left-[1px]">
+          <p className=" xl:leading-[40px] font-rosario magP text-[10px]   md:leading-[30px] md:mt-[-10px] md:pt-[10px] lg:pt-0 lg:text-[28px] xl:text-[32px] 2xl:text-[33px] lg:mt-[10px] lg:px-[10px]  lg:ml-[20px] md:ml-[20px] ml-[-15px] xl:mt-[0px] scale-95 md:scale-100 relative xl:left-[1px]">
             Stirrers with magnetic bars can stir liquids without a physical
             <br />
             connection. The magnetic bar is placed in the liquid, and a magnetic
@@ -27,11 +30,11 @@ function Magnetic() {
           </p>
         </div>
       </div>
-      <div className="md:block hidden magnet absolute  md:right-[-30px] right-[-37px] lg:right-[-40px] 2xl:right-[40px] xl:mt-[-125px] 2xl:mt-[-120px] lg:mt-[-120px] md:mt-[-95px] mt-[-60px] ">
+      <div className="md:block hidden magnet absolute  md:right-[-30px] right-[-37px] lg:right-[-40px] 2xl:right-[40px] xl:mt-[-125px] 2xl:mt-[-120px] lg:mt-[-120px] md:mt-[-95px] mt-[-60px] 2xl:scale-100 xl:scale-75 xl:right-[-110px] ">
         <img
           src={magnet}
           alt="magnet"
-          className="xl:h-[450px] xl:w-[450px] lg:h-[400px] lg:w-[400px] md:w-[280px] w-[135px] md:h-full h-[178px] scale-75 md:scale-100"
+          className="2xl:h-[450px] 2xl:w-[450px] xl:h-[470px] xl:w-[462px] lg:h-[400px] lg:w-[400px] md:w-[280px] w-[135px] md:h-full h-[178px] scale-75 md:scale-100 relative xl:bottom-2 2xl:bottom-0 xl:left-1 2xl:left-0"
         />
       </div>
     </div>
