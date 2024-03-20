95c165033beb8a9c4fb89f04592eccaf04ed6769
upulkr <upulkr4@gmail.com>
2024-03-20 19:13:15 +0530

    scrooltop

--- src/pages/Furniture/FurnitureMainPage.jsx
+++ src/pages/Furniture/FurnitureMainPage.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ProductTemplate from "../../components/ProductTemplate";
 import { useParams } from "react-router-dom";
 import furniturebg from "../../assets/images/products/cateogories/furniture/furniturebg.png";
 import furnitureDetail from "../../assets/images/products/cateogories/furniture/furnitureDetails.png";
 import client from "../../assets/images/products/cateogories/furniture/client.png";
 export default function FurnitureMainPage() {
-
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   const { id } = useParams();
 
   return (
