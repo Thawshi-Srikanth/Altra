3eeda16406093cdcfc7a3014bae197f1cbf41a9e
upulkr <upulkr4@gmail.com>
2024-04-01 18:26:24 +0530

    mage

--- src/components/ImageCommonBg.jsx
+++ src/components/ImageCommonBg.jsx
@@ -6,12 +6,17 @@ function ImageCommonBg({ img, text }) {
   const { actualClient, } = userStateContext();
 
   const filtered = img.filter((item) =>{    return  item.title === actualClient});
-
+console.log(actualClient)
   return (
     <div className="relative">
       <div className=" grid grid-cols-2 absolute items-center mx-auto justify-center">
         {" "}
-        <p className="font-medium xl:w-[727px] w-[217px] lg:w-[570px] md:w-[410px] sm:w-[370px] h-[117px] xl:h-[328px] xl:top-[60px] relative items-center mx-auto justify-center  text-[7px] md:text-[15px] sm:text-[13px] lg:text-[20px] xl:text-[23px] font-rosario  bottom-0 inset-x-0  text-center   sm:top-[60px] 2xl:left-[100px] px-2 sm:px-0  top-[50px]  sm:left-3  md:mx-[3px] lg:mx-5 md:top-[90px]  xl:px-8  xl:text-wrap  ">
+        <p
+          className={`${actualClient === "geyer" ? "xl:top-[130px]" : ""} 
+        ${actualClient === "water" ? "xl:top-[60px]" : ""} 
+        
+        font-medium xl:w-[727px] w-[217px] lg:w-[570px] md:w-[410px] sm:w-[370px] h-[117px] xl:h-[328px] 2xl:top-[60px] relative items-center mx-auto justify-center  text-[7px] md:text-[15px] sm:text-[13px] lg:text-[20px] xl:text-[23px] font-rosario  bottom-0 inset-x-0  text-center   sm:top-[60px] 2xl:left-[100px] xl:left-[-20px] px-2 sm:px-0  top-[50px]  sm:left-3  md:mx-[3px] lg:mx-5 md:top-[90px]  xl:px-8  xl:text-wrap `}
+        >
           {text}
         </p>
       </div>
