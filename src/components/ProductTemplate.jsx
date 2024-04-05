6a18f6489fa91a212fcd78c406689a335d066eae
upulkr <upulkr4@gmail.com>
2024-04-05 13:29:39 +0530

    equip,ent page

--- src/components/ProductTemplate.jsx
+++ src/components/ProductTemplate.jsx
@@ -110,7 +110,7 @@ console.log(pathName)
                 ${
                   MainNextpath === "equipment" &&
                   [1, 3, 5, 7, 9].includes(client.id)
-                    ? "rounded-md w-[100px] h-[89px]  lg:h-[171px]  md:h-[101px] sm:h-[101px] lg:w-[206px] md:w-[116px] sm:w-[146px]  xl:max-w-[240px]   scale-x-75 sm:scale-100"
+                    ? "rounded-md w-[100px] h-[89px]  lg:h-[171px]  md:h-[101px] sm:h-[101px] lg:w-[206px] md:w-[116px] sm:w-[146px]  xl:max-w-[240px] xl:h-[171px]  scale-x-75 sm:scale-100"
                     : "rounded-md xl:w-[342px]  xl:h-[171px] md:h-[101px] sm:h-[101px] lg:h-[171px]  "
                 } 
                    ${
@@ -154,7 +154,7 @@ console.log(pathName)
                        : " "
                    }
                    ${
-                     MainNextpath === "equipment" 
+                     MainNextpath === "equipment"
                        ? "xl:w-[342px] xl:h-[172px]"
                        : " "
                    }
