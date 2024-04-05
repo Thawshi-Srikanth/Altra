ad51f021549a6aedf17d802dd9100677d16ff548
upulkr <upulkr4@gmail.com>
2024-04-06 00:24:56 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -88,7 +88,7 @@ console.log(currentTab)
         <div
           className={`bg-white  z-50 top-0 bottom-0 w-screen  relative  lg:block hidden  lg:flex-row lg:h-[100px] lg:shadow-[10px_10px_10px_rgba(0,_0,_0,_0.3)] `}
         >
-          <div className="lg:right-[10px] xl:right-0 lg:flex lg:flex-row xl:pl-[86px] xl:pr-[86px] lg:pt-[10px] lg:pb-[10px] relative xl:left-[20px] 2xl:left-[50px]">
+          <div className="lg:right-[10px] xl:right-0 lg:flex lg:flex-row xl:pl-[86px] xl:pr-[86px] lg:pt-[10px] lg:pb-[10px] relative xl:left-[20px] 2xl:left-[90px]">
             <Link to="/">
               <div className="lg:flex lg:flex-row lg:scale-75 xl:scale-100 relative xl:right-[60px] 2xl:right-[70px]  ">
                 <img
