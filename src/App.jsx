9e6075f21cd95bd9e0ff30cf0783906e034bfc76
upulkr <upulkr4@gmail.com>
2024-04-08 01:55:03 +0530

    vercel error

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import AboutUs from "./pages/AboutUs";
 import ChemicalasMainPage from "./pages/Chemicals&Standards/ChemicalasMainPage";
 import ConsumablesMainPage from "./pages/Consumables/ConsumablesMainPage";
 import Contact from "./pages/Contact";
-import EquipmentMainPage from "./pages/Equipments/EquipmentMainPage";
+
 import FurnitureMainPage from "./pages/Furniture/FurnitureMainPage";
 import GlasswareMainPage from "./pages/Glassware/GlasswareMainPage";
 import NotFoundPage from "./pages/NotFoundPage";
@@ -17,9 +17,7 @@ import PlasticwareMainPage from "./pages/Plasticware/PlasticwareMainPage";
 import Products from "./pages/Products";
 import Research from "./pages/Research";
 import Services from "./pages/Services";
-
-
-
+import EquipmentMainPage from "./pages/Equipments/EquipmentMainPage"
 export default function App() {
   return (
     <Router>
@@ -28,17 +26,29 @@ export default function App() {
       <Routes>
         <Route path="*" element={<NotFoundPage />}></Route>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/products" element={<Products />}/>
+        <Route path="/products" element={<Products />} />
         <Route path="/services" element={<Services />}></Route>
         <Route path="/research" element={<Research />}></Route>
         <Route path="/aboutUs" element={<AboutUs />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
-  
-         <Route path="/products/equipment" element={<EquipmentMainPage />}></Route>
-      
-        <Route path="/products/furniture" element={<FurnitureMainPage />}></Route>
-        <Route path="/products/glassware" element={<GlasswareMainPage />}></Route>
-        <Route path="/products/chemicals" element={<ChemicalasMainPage />}></Route>
+
+        <Route
+          path="/products/equipment"
+          element={<EquipmentMainPage />}
+        ></Route>
+
+        <Route
+          path="/products/furniture"
+          element={<FurnitureMainPage />}
+        ></Route>
+        <Route
+          path="/products/glassware"
+          element={<GlasswareMainPage />}
+        ></Route>
+        <Route
+          path="/products/chemicals"
+          element={<ChemicalasMainPage />}
+        ></Route>
         <Route
           path="/products/plasticware"
           element={<PlasticwareMainPage />}
@@ -46,7 +56,7 @@ export default function App() {
         <Route
           path="/products/consumables"
           element={<ConsumablesMainPage />}
-        ></Route> 
+        ></Route>
 
         <Route
           path="/products/:mainNextpath/:clientNextpath"
